236f1fa033f84ebaf106cbe250c100b888e9ee38
Heidi Cao <heidihcao@gmail.com>
2021-09-19 16:56:18 -0700

    final version of lab 7

--- app3.js
+++ app3.js
@@ -1,15 +1,20 @@
 let usersee= prompt('Would you like to see more?')
 if (usersee.toLowerCase() == 'yes'){
-    document.write('Link to External Site')
+    document.write('Link to External Site <br>')
 }
 
-let usernumberanswer = parseInt(prompt('Please enter your favorite number between 1-9'));
-
-function getusernumberonetonine(){
-    if (usernumberanswer > 9 || usernumberanswer < 0) {
-        usernumberanswer = parseInt(prompt('Hmhmhm...you might have made an error!'))
-    }
-}
-getusernumberonetonine ();
 
 // OR operator is denoted by ||
+
+let usergallery = parseInt(prompt('How many times in the past year have you seen an art show or been to a museum?'));
+
+function getgallery(){
+    if (usergallery > 5){
+        document.write('WOW! You are a connoisseur!!')
+    } else{
+        document.write('Can we help you find you some local shows and galleries?')
+    }
+
+}
+
+getgallery();
